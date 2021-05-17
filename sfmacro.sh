1d33e074e03b5bc127b2ab6e889a75625db9a26a
Dz0526 <shining.sun0526@gmail.com>
2021-05-17 17:53:32 +0900

    feat(sfmacro): complete vs compailer function

--- sfmacro.sh
+++ sfmacro.sh
@@ -2,4 +2,7 @@
 
 sed -i '1i #define scanf_s(fmt, ... ) scanf(fmt, __VA_ARGS__ )' $1
 sed -i '2i #define strcpy_s(s, data) strcpy(s, data)' $1
+sed -i '3i #define sscanf_s(fmt, ... ) sscanf(fmt, __VA_ARGS__ )' $1
+sed -i '4i #define rewind(s) clear()' $1
+sed -i '5i #define gets_s(s, num) fgets(s, num, stdin)' $1
 sed -e "s/\r//g" $1 > $2
